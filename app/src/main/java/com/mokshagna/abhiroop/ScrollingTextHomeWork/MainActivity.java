f5796aa4cb697efb78b5600e9a90c73051b87cda
Abhiroop mokshagna <abhiroopmokshagnabheemineni@gmail.com>
2019-06-09 10:05:07 +0530

    Scrolling Text With ContextMenu

--- app/src/main/java/com/mokshagna/abhiroop/ScrollingTextHomeWork/MainActivity.java
+++ app/src/main/java/com/mokshagna/abhiroop/ScrollingTextHomeWork/MainActivity.java
@@ -2,6 +2,12 @@ package com.mokshagna.abhiroop.ScrollingTextHomeWork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView articleText = findViewById(R.id.article);
+        registerForContextMenu(articleText);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.context_edit:
+                displayToast("Edit");
+                return true;
+            case R.id.context_share:
+                displayToast("Share");
+                return true;
+            case R.id.context_delete:
+                displayToast("Delete");
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    public void displayToast(String message){
+        Toast.makeText(getApplicationContext(),"You Selected: "+message,Toast.LENGTH_SHORT).show();
     }
 }
